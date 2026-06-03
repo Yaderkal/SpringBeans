006fe7b17d07e1e0e11adf792ea6f034acff04ac
Yaderkal <amesheryakov22813337@mail.ru>
2026-06-03 23:36:22 +0300

    commit

--- PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,22 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
 
 @Component
 public class AnimalsCage {
 
+    @Qualifier("dog")
     @Autowired
     private Animal animal;
+    @Qualifier("cat")
+    @Autowired
+    private Animal animal1;
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +24,15 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+        System.out.println("Say:");
+        System.out.println(animal1.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
+
---
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/BeanTimer.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+@Component
+public class BeanTimer extends Timer {
+
+
+    @Override
+    public Long getTime() {
+        return super.getTime();
+    }
+}
