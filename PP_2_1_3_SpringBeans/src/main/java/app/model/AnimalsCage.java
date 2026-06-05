57d1bd28814fd94e906982698768e26e294aa5aa
mesheryak <amesheryakov2281333@mail.ru>
2026-06-05 17:11:39 +0300

    commit

--- PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
@@ -22,12 +22,12 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
         System.out.println("Say:");
         System.out.println(animal1.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
